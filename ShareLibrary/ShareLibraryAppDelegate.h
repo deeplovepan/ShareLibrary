e5ddc7c73cce468b067b6371f61d23226380c611
Peter Pan <deeplovepan@gmail.com>
2012-03-13 19:20:56 +0800

    Initial Commit

---
+++ ShareLibrary/ShareLibraryAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShareLibraryAppDelegate.h
+//  ShareLibrary
+//
+//  Created by Peter Pan on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareLibraryAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareLibrary/ShareLibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareLibraryViewController.h
+//  ShareLibrary
+//
+//  Created by Peter Pan on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareLibraryViewController : UIViewController
+
+@end
